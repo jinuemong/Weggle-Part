b9186d66b8e6b5d6f018bbdf50740bddeb71b025
jinuemong <jinwoo3661@naver.com>
2023-01-03 17:41:37 +0900

    2023 01 03

--- app/src/main/java/kr/co/ky/weggle/MainActivity.kt
+++ app/src/main/java/kr/co/ky/weggle/MainActivity.kt
@@ -2,11 +2,33 @@ package kr.co.ky.weggle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.navigation.NavigationView
+import kr.co.ky.weggle.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener{
+    private lateinit var binding :ActivityMainBinding
+    lateinit var frManger: FragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //test
+        setContentView(binding.root)
+        frManger = this@MainActivity.supportFragmentManager
     }
+
+    private fun initView(){
+
+    }
+
+    private fun setUpListener(){
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
