aa93ed987bd695b507626ea2596757274afdef8c
jinuemong <jinwoo3661@naver.com>
2023-01-23 23:58:03 +0900

    20230123

--- app/src/main/java/com/puresoftware/bottomnavigationappbar/Weggler/Server/WegglerRetrofitService.kt
+++ app/src/main/java/com/puresoftware/bottomnavigationappbar/Weggler/Server/WegglerRetrofitService.kt
@@ -1,9 +1,9 @@
 package com.puresoftware.bottomnavigationappbar.Weggler.Server
 
 import com.puresoftware.bottomnavigationappbar.Weggler.Model.Community
+import com.puresoftware.bottomnavigationappbar.Weggler.Model.MultiCommunityData
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 //레트로핏 연결 url 관리 (weggler)
 
@@ -15,4 +15,11 @@ interface WegglerRetrofitService {
         @Query(value = "page",encoded=true)page:Int,
         @Query(value = "size",encoded=true)size:Int,
     ): Call<Community>
+
+    //post community post data
+    @POST("boards/community/posts/")
+    @FormUrlEncoded
+    fun addCommunityPost(
+        @Field("body")body: MultiCommunityData
+    ): Call<MultiCommunityData>
 }
