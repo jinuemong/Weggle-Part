34fa5d8a6f811a80954dfd2c30fdc6e236c7b984
김진우 <jinwoo3661@naver.com>
2023-02-15 02:07:02 +0900

    20230215

---
+++ app/src/main/java/com/puresoftware/bottomnavigationappbar/Weggler/Unit/FragmentFactory.kt
@@ -0,0 +1,16 @@
+package com.puresoftware.bottomnavigationappbar.Weggler.Unit
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import com.puresoftware.bottomnavigationappbar.Weggler.SideFragment.AddCommunity.GallerySlideFragment
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
+
+class galleryFragmentFactory(private val mainFrame:SlidingUpPanelLayout): FragmentFactory() {
+    // 빈 생성자
+    override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+        return when (className){
+            GallerySlideFragment::class.java.name -> GallerySlideFragment(mainFrame)
+            else -> super.instantiate(classLoader, className)
+        }
+    }
+}
